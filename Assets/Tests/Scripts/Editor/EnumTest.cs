3ce7e12db5ceaa93ee67339b985f10a3680c13c8
Tetsuya Mori <monry84@gmail.com>
2017-07-21 12:41:04 +0900

    :new: テストコード追加

---
+++ Assets/Tests/Scripts/Editor/EnumTest.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+
+namespace Tests {
+
+    public class EnumTest {
+
+        private enum SampleEnum {
+            Default,
+            Succeed,
+            Failure,
+        }
+
+        [Test]
+        public void TryParseTest() {
+            SampleEnum sampleEnumSucceed;
+            Assert.IsTrue(ForwardCompatibility.Enum.TryParse("Succeed", out sampleEnumSucceed), "パース成功");
+            Assert.AreEqual(SampleEnum.Succeed, sampleEnumSucceed, "パース結果チェック: 成功");
+
+            SampleEnum sampleEnumFailure;
+            Assert.IsFalse(ForwardCompatibility.Enum.TryParse("Failed", out sampleEnumFailure), "パース失敗");
+            Assert.AreNotEqual(SampleEnum.Failure, sampleEnumFailure, "パース結果チェック: 失敗: Failure と不一致");
+            Assert.AreEqual(SampleEnum.Default, sampleEnumFailure, "パース結果チェック: 失敗: Default と一致");
+        }
+
+    }
+
+}
